e7ab1275040bc44aaa174fc0ca45e3782284c627
Nicola Scendoni <nscendoni@adobe.com>
2025-12-09 17:57:45 +0100

    First commit

--- core/src/main/java/com/wintergw2025/core/models/HelloWorldModel.java
+++ core/src/main/java/com/wintergw2025/core/models/HelloWorldModel.java
@@ -18,19 +18,24 @@ package com.wintergw2025.core.models;
 import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
 
 import javax.annotation.PostConstruct;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 
+import java.util.Iterator;
 import java.util.Optional;
 
 @Model(adaptables = Resource.class)
@@ -48,15 +53,31 @@ public class HelloWorldModel {
     private String message;
 
     @PostConstruct
-    protected void init() {
+    protected void init() throws RepositoryException {
         PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
         String currentPagePath = Optional.ofNullable(pageManager)
                 .map(pm -> pm.getContainingPage(currentResource))
                 .map(Page::getPath).orElse("");
 
+        Session session = resourceResolver.adaptTo(Session.class);
+        UserManager um = resourceResolver.adaptTo(UserManager.class);
+        Authorizable user = um.getAuthorizable(session.getUserID());
+        
         message = "Hello World!\n"
-            + "Resource type is: " + resourceType + "\n"
-            + "Current page is:  " + currentPagePath + "\n";
+                + "Resource type is: " + resourceType + "\n"
+                + "Current page is:  " + currentPagePath + "\n"
+                + "Logged User:\n" +
+                "Path: " + user.getPath() + "\n" +
+                "PrincipalName: " + user.getPrincipal().getName() + "\n" +
+                "givenName:" + user.getProperty("profile/given_name") + "\n" +
+                "surname:" + user.getProperty("profile/family_name") + "\n" +
+                "Groups: \n";
+        message += "<ul>\n";
+        Iterator<Group> memberOf = user.memberOf();
+        while (memberOf.hasNext()) {
+            message = message + "<li>" + memberOf.next().getPath() + "\n";
+        }
+        message += "</ul>\n";
     }
 
     public String getMessage() {
