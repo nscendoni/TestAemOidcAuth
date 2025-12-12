48166d7337c8301ded14cb45fd6d8aa222a2bbde
Nicola Scendoni <nscendoni@adobe.com>
2025-12-12 10:03:39 +0100

    Added url with secret content.

---
+++ core/src/main/java/com/wintergw2025/core/servlets/SecretDataServlet.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2015 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.wintergw2025.core.servlets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.propertytypes.ServiceDescription;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Servlet that returns secret JSON data.
+ * Accessible at: /bin/wintergw2025/secret-data
+ */
+@Component(service = { Servlet.class })
+@SlingServletPaths("/bin/wintergw2025/secret-data")
+@ServiceDescription("Secret Data Servlet")
+public class SecretDataServlet extends SlingSafeMethodsServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest req,
+            final SlingHttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("{\"content\": \"this data is very secret\"}");
+    }
+}
+
+
